bcfcb889c22f2b2520bbb4236339f0a160f31cca
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-03-15 10:55:41 -0400

    IDI-3: First pass at modifying theme to be child of wordpress-fss-theme

---
+++ footer-content.php
@@ -0,0 +1,3 @@
+		<footer role="contentinfo" class="fl-site-footer fl-centered fl-push">
+			The Inclusive Design Institute is awesome.
+		</footer>
--- idi.js
+++ idi.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2011 OCAD University
+Copyright 2012 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
@@ -15,42 +15,27 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 // JSLint options 
 /*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
 var idi = idi || {};
+
 (function ($) {
+    idi.makeTopNavSticky = function () {
+        var theWindow = $(window);
 
-/*    fluid.demands("fluid.uiOptions.templatePath", "fluid.fatPanelUIOptions", {
-        options: {
-            value: "{fatPanelUIOptions}.options.prefix"
-        }
-    });
-    
-    fluid.demands("fluid.renderIframe", ["fluid.fatPanelUIOptions"], {
-        options: {
-            markupProps: {
-                src: "http://dev.inclusivedesign.ca/wordpress/wp-content/themes/idi_theme/webapp/components/uiOptions/html/FatPanelUIOptionsFrame.html"
-            }
-        }
-    });
+        var topNavEl = $('.fl-site-nav-main');
+        var topNavHeight = topNavEl.css('height');
 
-    // Supply the table of contents' template URL
-    fluid.demands("fluid.tableOfContents.levels", "fluid.tableOfContents", {
-        options: {
-            resources: {
-                template: {
-                    forceCache: true,
-                    url: "http://dev.inclusivedesign.ca/wordpress/wp-content/themes/idi_theme/webapp/components/tableOfContents/html/TableOfContents.html"
-                }
-            }
-        }
-    });*/
+        var topNavTop = topNavEl.offset().top;
+        var spacerEl = $("<div class='fl-spacer-el'></div>");
+        spacerEl.css("height", topNavHeight);
+        spacerEl.insertBefore(topNavEl);
 
-    idi.init = function () {        
-		fluid.pageEnhancer({
-			//tocTemplate: "<?php bloginfo('template_url'); ?>/infusion/components/tableOfContents/html/TableOfContents.html"
-			tocTemplate: "http://dev.inclusivedesign.ca/wordpress/wp-content/themes/idi_theme/infusion/components/tableOfContents/html/TableOfContents.html"
-		});
-
-		fluid.uiOptions.fatPanel(".flc-uiOptions-fatPanel", {
-            prefix: "http://dev.inclusivedesign.ca/wordpress/wp-content/themes/idi_theme/infusion/components/uiOptions/html/"			
-		});          
+        theWindow.scroll(function () {
+            var windowTop = theWindow.scrollTop();
+            spacerEl.toggle(windowTop > topNavTop);
+            topNavEl.toggleClass('sticky', windowTop > topNavTop);
+        });
     };
+
+    $(document).ready(function () {
+        idi.makeTopNavSticky();
+    });
 })(jQuery);
