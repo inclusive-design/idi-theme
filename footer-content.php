52eba2cf36e9531a9bb875c84cc2ba1c2ba69f68
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-04-17 13:28:00 -0400

    IDI-4: Implement basic content on front page.

--- footer-content.php
+++ footer-content.php
@@ -1,61 +1,11 @@
-		<script type="text/javascript">
-			function submitForm() {
-				var listForm = $('#myForm');
-				var listEmail = $('#listEmail').val();
-
-				//validate for non-html5 browsers
-				var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-			    if(reg.test(listEmail) == false) {
-					alert("Invalid email");			//switch to accessible html?
-				} else {
-					$.ajax({
-						url: listForm.attr('action'),
-						type: listForm.attr('method'),
-						data: {email: listEmail},
-						success: function(email) {
-							$("#listForm").html("<p>Success! " + email + " has been added to the mailing list. You should receive a confirmation email shortly.</p>"); 	
-						}
-					});
-
-				}
-				return false;
-			}
-		</script>
 
 		<footer class="fl-centered fl-site-footer">
-			<div class="fl-col-flex2 idi-footer-main">
-				<div class="fl-col">
-					Inclusive Design Institute
-					<div class="fl-col-flex2">
-						<div class="fl-col"><span class="address">205 Richmond St. West 2nd Floor<br/>
-							                Toronto ON, M5V 1V3, Canada</span>
-						</div>
-						<div class="fl-col"><span class="phone-number">416.977.6000 ext. 3968</span><br/>
-											<span class="email">idi@ocadu.ca</span>
-						</div>
-					</div>
-				</div>
-				<div class="fl-col">
-					Funded by the Canada Foundation for Innovation
-					<div class="fl-col-flex2">
-						<div class="fl-col">
-							<ul>
-								<li>OCAD University</li>
-								<li>Ryerson University</li>
-								<li>York University</li>
-								<li>OUIT</li>
-							</ul>
-						</div>
-						<div class="fl-col">
-							<ul>
-								<li>University of Toronto</li>
-								<li>Sheridan College</li>
-								<li>George Brown College</li>
-								<li>Seneca College</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-
+			<div class="idi-contact-info">
+				205 Richmond Street West, 2nd Floor, Toronto, ON M5V 1V3, Canada
+				<br/>
+				(416) 977-6000, x3968 / <a href="mailto:idi@ocadu.ca">idi@ocadu.ca</a>
+			</div>
+			<div class="idi-cfi">
+				Funded by the <a href="">Canada Foundation for Innovation</a>
 			</div>
 		</footer>
--- front-page.php
+++ front-page.php
@@ -40,7 +40,7 @@ Template Name: Front Page
 			$more = 0; 
 			?>
 			<div class="fl-col">
-				<div class="box post">
+				<div class="idi-shadow-box post">
 				    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			        <div class="date"><?php the_time('F jS, Y') ?></div>
 		            <div class="entry">
@@ -54,41 +54,83 @@ Template Name: Front Page
 // This page uses The Twitter Feed Wordpress Plugin found at
 //     http://pleer.co.uk/wordpress/plugins/wp-twitter-feed/
 
-$twitter_feed_opts = ' followlink="no" num="1" img="no" tprefix="" tsuffix="ago" other=”yes”';
+$twitter_feed_opts = ' followlink="no" num="1" linktotweet="no" tweetintent="no" img="no" tprefix="" tsuffix="ago" other=”yes” ulclass="twitter-list" liclass="twitter-tweet"';
 ?>
 
 		<div class="fl-col">
-			<div class="box twitter-feed-group">
+			<div class="idi-shadow-box twitter-feed-group">
 				<a class="twitter-follow-button" rel="external nofollow" href="http://twitter.com/aegisproj">@aegisproj</a>
 				<?php echo do_shortcode('[twitter-feed username="aegisproj"' . $twitter_feed_opts . ']'); ?>
 			</div>
 
-			<div class="box twitter-feed-group">
-				<a class="twitter-follow-button" rel="external nofollow" href="http://twitter.com/FluidProject">@FluidProject</a>
-				<?php echo do_shortcode('[twitter-feed username="FluidProject"' . $twitter_feed_opts . ']'); ?>
+			<div class="idi-shadow-box twitter-feed-group">
+				<a class="twitter-follow-button" rel="external nofollow" href="http://twitter.com/SNOWocad">@SNOWocad</a>
+				<?php echo do_shortcode('[twitter-feed username="SNOWocad"' . $twitter_feed_opts . ']'); ?>
 			</div>
 
 		</div>
 
 		<div class="fl-col">
-			<div class="box twitter-feed-group">
-				<a class="twitter-follow-button" rel="external nofollow" href="http://twitter.com/SNOWocad">@SNOWocad</a>
-				<?php echo do_shortcode('[twitter-feed username="SNOWocad"' . $twitter_feed_opts . ']'); ?>
+			<div class="idi-shadow-box twitter-feed-group">
+				<a class="twitter-follow-button" rel="external nofollow" href="http://twitter.com/FluidProject">@FluidProject</a>
+				<?php echo do_shortcode('[twitter-feed username="FluidProject"' . $twitter_feed_opts . ']'); ?>
 			</div>
 
-			<div>
-				IDI Institution list,
-				contact us,
+			<div class="idi-institutions idi-box">
+				<h3>IDI Institutions</h3>
+				<p>Find out more about their ongoing research</p>
+				<ul class="idi-institution-list">
+					<li><a href="http://ocadu.ca">OCAD University</a></li>
+					<li><a href="http://ocadu.ca">Ryerson University</a></li>
+					<li><a href="http://ocadu.ca">York University</a></li>
+					<li><a href="http://ocadu.ca">UOIT</a></li>
+					<li><a href="http://ocadu.ca">University of Toronto</a></li>
+					<li><a href="http://ocadu.ca">Sheridan College</a></li>
+					<li><a href="http://ocadu.ca">George Brown College</a></li>
+					<li><a href="http://ocadu.ca">Seneca College</a></li>
+				</ul>
+				<h3>Contact us!</h3>
+				<div class="idi-mailing-address">
+					Inclusive Design Institute
+					205 Richmond Street West
+					2nd Floor
+					Toronto, ON M5V 1V3
+					Canada
+				</div>
+				<div class="idi-phone">
+					(416) 977-6000, x3968
+				</div>
+				<div class="idi-email">
+					<a href="mailto:idi@ocadu.ca">idi@ocadu.ca</a>
+				</div>
+				<script type="text/javascript">
+					function submitForm() {
+						var listForm = $('#myForm');
+						var listEmail = $('#listEmail').val();
+
+						//validate for non-html5 browsers
+						var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+					    if(reg.test(listEmail) == false) {
+							alert("Invalid email");			//switch to accessible html?
+						} else {
+							$.ajax({
+								url: listForm.attr('action'),
+								type: listForm.attr('method'),
+								data: {email: listEmail},
+								success: function(email) {
+									$("#listForm").html("<p>Success! " + email + " has been added to the mailing list. You should receive a confirmation email shortly.</p>"); 	
+								}
+							});
+
+						}
+						return false;
+					}
+				</script>
 				<div class="idi-mailing-list">
-					<div id="listForm">
-						<div>
-							Stay updated with our mailing list
-						</div>
-						<form id="myForm" method="post" action="<?php bloginfo('stylesheet_directory'); ?>/mailinglist.php" onsubmit="return submitForm()">
-							<input type="email" name="listEmail" id="listEmail" placeholder="your email address" required />
-							<input type="submit" value="submit" />
-						</form>
-					</div>
+					Mailing list sign up
+					<form id="idiMailingListSignup" method="post" action="<?php bloginfo('stylesheet_directory'); ?>/mailinglist.php" onsubmit="return submitForm()">
+						<input type="email" name="listEmail" id="listEmail" placeholder="Your email address" required />
+					</form>
 				</div>
 			</div>
 		</div>
