859aea15c82b2f65f9ca3a24d2f3b2e3b2b64f7f
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-04-19 10:06:14 -0400

    IDI-9: First draft of News pages

---
+++ sidebar-news.php
@@ -0,0 +1,32 @@
+<div>
+	<form class="idi-search-form" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+	        <input class="idi-search-field" type="text" value="" name="s" id="s" placeholder="search"/>
+	</form>
+
+	<div class="idi-article-list">
+		<h3>Newest articles</h3>
+		<ul>
+		<?php
+		query_posts('posts_per_page=5'); // query to show all posts independant from what is in the center;
+		if (have_posts()) :
+		   while (have_posts()) :
+		      the_post(); ?>
+		<li>
+		      <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+		</li>
+		<?php  endwhile;
+		endif;
+		wp_reset_query();
+		?>
+		</ul>
+	</div>
+
+	<div class="idi-mailing-list-reminder">
+		<h3>Stay updated,<br/>join our mailing list:</h3>
+		<?php get_template_part("mailing-list-form"); ?>
+	</div>
+
+	<?php get_template_part('tweets', 'aegisprog') ?>
+	<?php get_template_part('tweets', 'SNOWocad') ?>
+	<?php get_template_part('tweets', 'FluidProject') ?>
+</div>
