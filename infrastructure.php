29af779838dead8a70ce8f387ac231efe4c45f80
Cindy Qi Li <cli@ocad.ca>
2012-04-19 12:44:38 -0400

    IDI-11: Implement the infrastructure page.

--- infrastructure.php
+++ infrastructure.php
@@ -6,7 +6,145 @@ Template Name: Infrastructure
 <?php get_header(); ?>
 
 <div class="fl-centered fl-col-mixed fl-site-wrapper">
-	This is the infrastructure page.
+	<table class="idi-infra-table idi-noborder">
+		<tbody>
+		<tr>
+			<td></td>
+			<td colspan="3">
+				<h1 class="idi-infra-h1">Equipment and space booking, calendering, and more coming soon.</h1>
+			</td>
+		</tr>
+	
+		<tr>
+			<td></td>
+			<td>
+				<img class="idi-infra-campus-image" src="" alt="An overview picture of 205 richmond street west" title="An overview picture of 205 richmond street west" />
+			</td>
+			<td>
+				<img class="idi-infra-campus-image" src="" alt="An overview picture of 230 richmond street west" title="An overview picture of 230 richmond street west" />
+			</td>
+			<td>
+				<img class="idi-infra-campus-image" src="" alt="An overview picture of 49 McCaul street" title="An overview picture of 49 McCaul street" />
+			</td>
+		</tr>
+	
+		<tr>
+			<td class="idi-summary-col">
+				<h2 class="idi-summary-title">Facilities</h2>
+				<img src="" alt="A picture of facility." />
+			</td>
+	
+			<td>
+				<h2 class="idi-infra-h2">205 Richmond St. W.<br />Toronto, Canada</h2>
+				
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Collaboration room</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for the collaboration room.</p>
+				</div>
+			
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Testing and usability lab</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for the testing and usability lab.</p>
+				</div>
+			</td>
+		
+			<td>
+				<h2 class="idi-infra-h2">230 Richmond St. W.<br />Toronto, Canada</h2>
+				
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Captioning lab</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for the captioning lab.</p>
+				</div>
+			
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Boardroom</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for the boardroom.</p>
+				</div>
+			</td>
+		
+			<td>
+				<h2 class="idi-infra-h2">49 McCaul St.<br />Toronto, Canada</h2>
+				
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Blackbox theatre</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for the blackbox theatre.</p>
+				</div>
+			
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Research space</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for the research space.</p>
+				</div>
+			</td>
+		</tr>
+	
+		<tr>
+			<td class="idi-summary-col">
+				<h2 class="idi-summary-title">Equipment</h2>
+				<img src="" alt="A picture of equipment." />
+			</td>
+	
+			<td>
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Equipment 1A</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for Equipment 1A.</p>
+				</div>
+			
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Equipment 1B</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for Equipment 1B.</p>
+				</div>
+			</td>
+		
+			<td>
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Equipment 2A</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for Equipment 2A.</p>
+				</div>
+			
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Equipment 2B</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for Equipment 2B.</p>
+				</div>
+			</td>
+		
+			<td>
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Equipment 3A</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for Equipment 3A.</p>
+				</div>
+			
+				<div class="idi-infra-item">
+					<h3 class="idi-infra-h3">Equipment 3B</h3>
+					<h4 class="idi-infra-h4"><span class="idi-infra-bookit">Book it</span></h4>
+					<h5 class="idi-infra-h5">Coming soon</h5>
+					<p>The description for Equipment 3B.</p>
+				</div>
+			</td>
+		</tr>
+		</tbody>
+	</table> 
 </div>
-
+		
 <?php get_footer(); ?>
