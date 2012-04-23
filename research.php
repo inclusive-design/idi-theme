2496083a62425a07fb4c1b88eaba7d9644b8a4ec
Cindy Qi Li <cli@ocad.ca>
2012-04-23 10:15:34 -0400

    IDI-5: Adjusted the use of html5 section elements according to the code review feedback.

--- research.php
+++ research.php
@@ -7,72 +7,74 @@ Template Name: Research Page
 
 <div class="fl-centered fl-col-mixed fl-site-wrapper">
 
-	<div class="fl-col-fixed fl-force-left"> 	
-		<?php get_sidebar('research'); ?>
-	</div>
+	<aside>
+		<div class="fl-col-fixed fl-force-left"> 
+			<?php get_sidebar('research'); ?>
+		</div>
+	</aside>
 
-    <div class="fl-col-flex idi-one-column idi-research">
+	<div class="fl-col-flex idi-one-column idi-research">
 		<a class="idi-breadcrumbs" href="news">RESEARCH CLUSTERS &gt;</a>
 		
 		<ul class="fl-grid">
 			<li>
-				<article class="idi-research-item">
-					<head>
-						<div>
-							<a><img src="" alt="A link picture to the research cluster of design and development." title="A link picture to the research cluster of design and development." /></a>
-						</div>
-						<h2>Design & Development</h2>
-					</head>
-					<section>
+				<section>
+					<div class="idi-research-item">
+						<head>
+							<div>
+								<a><img src="" alt="A link picture to the research cluster of design and development." title="A link picture to the research cluster of design and development." /></a>
+							</div>
+							<h2>Design & Development</h2>
+						</head>
 						<p>Suspendisse erat ipsum, iaculis adipiscing adipiscing elit. Sed at purus auctor mi luctus sodales. Nunc scelerisque ligula porta.</p>
 						<p><a class="idi-more" href="design">learn more</a></p>
-					</section>
-				</article>
+					</div>
+				</section>
 			</li>
 
 			<li>
-				<article class="idi-research-item">
-					<head>
-						<div>
-							<a><img src="" alt="A link picture to the research cluster of Business Case, Policies, Standards & Legislation." title="A link picture to the research cluster of Business Case, Policies, Standards & Legislation." /></a>
-						</div>
-						<h2>Business Case, Policies, Standards & Legislation</h2>
-					</head>
-					<section>
+				<section>
+					<div class="idi-research-item">
+						<head>
+							<div>
+								<a><img src="" alt="A link picture to the research cluster of Business Case, Policies, Standards & Legislation." title="A link picture to the research cluster of Business Case, Policies, Standards & Legislation." /></a>
+							</div>
+							<h2>Business Case, Policies, Standards & Legislation</h2>
+						</head>
 						<p>Suspendisse erat ipsum, iaculis adipiscing adipiscing elit. Sed at purus auctor mi luctus sodales. Nunc scelerisque ligula porta.</p>
 						<p><a class="idi-more" href="policies">learn more</a></p>
-					</section>
-				</article>
+					</div>
+				</section>
 			</li>
 
 			<li>
-				<article class="idi-research-item">
-					<head>
-						<div>
-							<a><img src="" alt="A link picture to the research cluster of Mobile & Pervasive Computing." title="A link picture to the research cluster of Mobile & Pervasive Computing." /></a>
-						</div>
-						<h2>Mobile & Pervasive Computing</h2>
-					</head>
-					<section>
+				<section>
+					<div class="idi-research-item">
+						<head>
+							<div>
+								<a><img src="" alt="A link picture to the research cluster of Mobile & Pervasive Computing." title="A link picture to the research cluster of Mobile & Pervasive Computing." /></a>
+							</div>
+							<h2>Mobile & Pervasive Computing</h2>
+						</head>
 						<p>Suspendisse erat ipsum, iaculis adipiscing adipiscing elit. Sed at purus auctor mi luctus sodales. Nunc scelerisque ligula porta.</p>
 						<p><a class="idi-more" href="mobile">learn more</a></p>
-					</section>
-				</article>
+					</div>
+				</section>
 			</li>
 
 			<li>
-				<article class="idi-research-item">
-					<head>
-						<div>
-							<a><img src="" alt="A link picture to the research cluster design and development." title="A link picture to the research cluster design and development." /></a>
-						</div>
-						<h2>Implementation & Information Practices</h2>
-					</head>
-					<section>
+				<section>
+					<div class="idi-research-item">
+						<head>
+							<div>
+								<a><img src="" alt="A link picture to the research cluster design and development." title="A link picture to the research cluster design and development." /></a>
+							</div>
+							<h2>Implementation & Information Practices</h2>
+						</head>
 						<p>Suspendisse erat ipsum, iaculis adipiscing adipiscing elit. Sed at purus auctor mi luctus sodales. Nunc scelerisque ligula porta.</p>
 						<p><a class="idi-more" href="implementation">learn more</a></p>
-					</section>
-				</article>
+					</div>
+				</section>
 			</li>
 		</ul>
 	</div>
