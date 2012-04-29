82b74fc65c977e27a0aadc2c5e86c966339bd13a
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-04-29 15:44:03 -0700

    IDI-19: Started, but real problems with opacity.

--- cluster.php
+++ cluster.php
@@ -5,7 +5,7 @@ Template Name: Research Cluster
 ?>
 <?php get_header(); ?>
 
-<div class="fl-centered fl-col-mixed fl-site-wrapper idi-cluster">
+<div class="fl-centered fl-col-mixed fl-site-wrapper idi-cluster idi-<?php echo $post->post_name; ?>">
 
 	<div class="idi-cluster-leads fl-col-fixed fl-force-left"> 	
 
