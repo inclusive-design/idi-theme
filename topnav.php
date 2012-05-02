61d11db0a97026274c783315502a82f93adf94c2
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-05-02 16:51:17 -0400

    IDI-22: Implement proper dividers for footer and sticky nav

--- topnav.php
+++ topnav.php
@@ -1,3 +1,6 @@
 			<nav role="navigation" class="fl-site-nav-main">
-				<?php idi_generate_top_nav(); ?>
+				<div class="idi-nav-links">
+					<?php idi_generate_top_nav(); ?>
+				</div>
+				<div class="idi-nav-links-buffer"></div>
 			</nav>
