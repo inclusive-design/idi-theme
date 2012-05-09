bc7749691590d8bb23a23961906a49a65704ef54
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-05-09 16:11:19 -0400

    IDI-15: Starting to describe infrastructure; fix to institution list

--- infrastructure.php
+++ infrastructure.php
@@ -40,7 +40,7 @@ Template Name: Infrastructure
 				<h3>Collaboration room</h3>
 				<h4><span class="idi-infra-bookit">Book it</span></h4>
 				<h5>Coming soon</h5>
-				<p>The description for the collaboration room.</p>
+				<p>The hub of the distributed collaboration network. Enables distributed participation by research team members, supporting resource sharing, collaborative development and both ongoing casual and scheduled communication in multiple modalities.</p>
 			</div>
 		
 			<div class="idi-infra-item">
@@ -58,14 +58,14 @@ Template Name: Infrastructure
 				<h3>Captioning lab</h3>
 				<h4><span class="idi-infra-bookit">Book it</span></h4>
 				<h5>Coming soon</h5>
-				<p>The description for the captioning lab.</p>
+				<p>An audio/video recording suite enabling research in captioning and description; Supports both post-production and real time captioning and description.</p>
 			</div>
 		
 			<div class="idi-infra-item">
 				<h3>Boardroom</h3>
 				<h4><span class="idi-infra-bookit">Book it</span></h4>
 				<h5>Coming soon</h5>
-				<p>The description for the boardroom.</p>
+				<p>An optimally accessible meeting room that supports remote and on-site multi-lingual translation, real time captioning and description, sign language interpretation and a variety of assistive listening devices.</p>
 			</div>
 		</div>
 		<div class="fl-col">
@@ -96,10 +96,10 @@ Template Name: Infrastructure
 		</div>
 		<div class="fl-col">
 			<div class="idi-infra-item">
-				<h3>Equipment 1A</h3>
+				<h3>Portable Accessible Performance Kit</h3>
 				<h4><span class="idi-infra-bookit">Book it</span></h4>
 				<h5>Coming soon</h5>
-				<p>The description for Equipment 1A.</p>
+				<p>Equipment to add remote and in-situ real-time captioning, description, sign translation and fm listening assistance to an existing architecturally accessible performance space.</p>
 			</div>
 		
 			<div class="idi-infra-item">
