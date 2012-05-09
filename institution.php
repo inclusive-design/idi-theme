745c28d699e31e884fc7919363092367dd2a98da
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-05-09 15:19:07 -0400

    IDI-8, IDI-15: Fleshing out a plan for the institution pages; sidebars for some, content for some

--- institution.php
+++ institution.php
@@ -5,12 +5,23 @@ Template Name: Institution
 ?>
 <?php get_header(); ?>
 
-<div class="fl-centered fl-col-mixed fl-site-wrapper">
-	<?php
-		$page_data = get_page(get_the_ID());
-		echo '<h1>'. $page_data->post_title .'</h1>';
-		echo apply_filters('the_content', $page_data->post_content);
-	?>
+<div class="fl-centered fl-site-wrapper fl-col-mixed idi-institution">
+	<aside class="fl-col-fixed fl-force-left">
+			<?php get_sidebar($post->post_name); ?>
+	</aside>
+
+
+	<div class="fl-col-flex idi-one-column">
+		<?php
+			$page_data = get_page(get_the_ID());
+			echo '<h1>'. $page_data->post_title .'</h1>';
+		?>
+
+		<div class="idi-inst-content">
+			<?php
+				echo apply_filters('the_content', $page_data->post_content);
+			?>
+		</div>
 </div>
-		
+
 <?php get_footer(); ?>
