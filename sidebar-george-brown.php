fcbf83e9f178a3919b7b0d41662f47e4ddd16448
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-05-11 10:56:29 -0400

    IDI-8: Add sidebars for GB, Sheridan, UT; fix styling of address icons

---
+++ sidebar-george-brown.php
@@ -0,0 +1,28 @@
+<div class="idi-inst-site-links">
+	<p><a class="idi-external-link" href="http://www.georgebrown.ca/research/">visit ORI site</a></p>
+	<p><a class="idi-external-link" href="http://georgebrown.ca">visit George Brown site</a></p>
+</div>
+
+<div class="idi-contact-info">
+	<h3>Contact</h3>
+	<div class="idi-mailing-address">
+		Office of Research and Innovation<br/>
+		George Brown College<br/>
+		<a href="http://g.co/maps/39unq">
+			160 Kendal Avenue</a><br/>
+		Room C302<br/>
+		Toronto, ON M5T 2T9<br/>
+		Canada
+	</div>
+	<div class="idi-phone">
+		(416) 415-5000, x6082
+	</div>
+</div>
+
+<h3>Leads</h3>
+<div class="idi-project-lead">
+	<span class="idi-project-lead-name">Robert Luke</span>
+	<div class="idi-project-lead-blurb">Director of Applied Research and Innovation</div>
+</div>
+
+<?php get_template_part("institution-list"); ?>
