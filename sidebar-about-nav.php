ef8fbb32579169ce7e01776629677dfd8c162229
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-05-22 12:17:22 -0400

    IDI-58: Styling the new About page (headers and left-sidebar nav).

--- sidebar-about-nav.php
+++ sidebar-about-nav.php
@@ -1,7 +1,7 @@
 <nav class="idi-about-sidebar">
 	<ul>
-	    <li><a href="#Overview">Overview</a></li>
-	    <li><a href="#Leadership">Leadership &amp; Governance</a>
+	    <li class="idi-nav-top-level"><a href="#Overview">Overview</a></li>
+	    <li class="idi-nav-top-level"><a href="#Leadership">Leadership &amp; Governance</a>
 			<ul>
 			    <li><a href="#Steering">Steering Committee</a></li>
 			    <li><a href="#Advisory">Advisory Committee</a></li>
@@ -10,9 +10,9 @@
 			    <li><a href="#Project">Project Manager</a></li>
 			</ul>
 	    </li>
-	    <li><a href="#Clusters">Research Clusters</a></li>
-	    <li><a href="#Capacity">Capacity Building</a></li>
-	    <li><a href="#Sustainability">Implementation and Sustainability</a></li>
+	    <li class="idi-nav-top-level"><a href="#Clusters">Research Clusters</a></li>
+	    <li class="idi-nav-top-level"><a href="#Capacity">Capacity Building</a></li>
+	    <li class="idi-nav-top-level"><a href="#Sustainability">Implementation and Sustainability</a></li>
 	</ul>
 </nav>
 
