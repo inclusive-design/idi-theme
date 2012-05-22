a465458279c1e191dc4443cde0c5f8408bd1efdf
Cindy Qi Li <cli@ocad.ca>
2012-05-22 13:57:26 -0400

    IDI-38: More IE8 fixes on smaller icons and layout styling.

--- cluster-projects-implementation.php
+++ cluster-projects-implementation.php
@@ -1,84 +1,100 @@
 		<div class="fl-col-flex2">
 			<div class="fl-col">
-				<section class="idi-project fl-col-mixed">
-					<div class="idi-project-logo fl-col-fixed fl-force-left">
-						<img class="idi-logo-placeholder" src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/logoplaceholder/logoplaceholder.png" title="Project logo placeholder" alt="Project logo placeholder" />
-					</div>
-					<div class="fl-col-flex">
-						<h3><a href="http://openvulab.org/">Open Virtual Usability Lab</a></h3>
-						OpenVULab is an open source tool designed for remote accessibility and usability testing. The tool is designed to capture remote users’ interactions with their computer screens as well as their microphone audio.
-					</div>
-				</section>
-				<section class="idi-project fl-col-mixed">
-					<div class="idi-project-logo fl-col-fixed fl-force-left">
-						<a class="idi-no-tab-focus" href="http://sites.google.com/site/decapodproject/">
-							<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/Decapod.png" title="Decapod project logo" alt="Decapod project logo"/>
-						</a>
-					</div>
-					<div class="fl-col-flex">
-						<h3><a href="http://sites.google.com/site/decapodproject/">Decapod</a></h3>
-						Decapod is a project focused on building a low-cost digitization solution that will allow for rare materials, materials held in collections without large budgets, and other scholarly content to be digitized into a high-quality PDF format. This project will work to incorporate the hardware and software necessary to accomplish this goal.
-					</div>
-				</section>
-				<section class="idi-project fl-col-mixed">
-					<div class="idi-project-logo fl-col-fixed fl-force-left">
-						<img class="idi-logo-placeholder" src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/logoplaceholder/logoplaceholder.png" title="Project logo placeholder" alt="Project logo placeholder" />
-					</div>
-					<div class="fl-col-flex">
-						<h3><a href="http://irlt.yorku.ca/">Compareware</a></h3>
-						Compareware is a game for 5-7 year olds that attempts to assist those with reading difficulties to 'level up' their vocabulary and their analytical skills in determining similarities and differences between two objects.
-					</div>
-				</section>
-				<section class="idi-project fl-col-mixed">
-					<div class="idi-project-logo fl-col-fixed fl-force-left">
-						<a class="idi-no-tab-focus" href="http://atutor.ca/">
-							<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/AT_Logo_1_sm.png" title="ATutor logo" alt="ATutor logo"/>
-						</a>
-					</div>
-					<div class="fl-col-flex">
-						<h3><a href="http://atutor.ca/">ATutor</a></h3>
-						ATutor is an Open Source Web-based Learning Management System (LMS) used to develop and deliver online courses.
-					</div>
-				</section>
+				<div class="idi-project fl-col-mixed">
+					<section>
+						<div class="idi-project-logo fl-col-fixed fl-force-left">
+							<img class="idi-logo-placeholder" src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/logoplaceholder/logoplaceholder.png" title="Project logo placeholder" alt="Project logo placeholder" />
+						</div>
+						<div class="fl-col-flex">
+							<h3><a href="http://openvulab.org/">Open Virtual Usability Lab</a></h3>
+							OpenVULab is an open source tool designed for remote accessibility and usability testing. The tool is designed to capture remote users’ interactions with their computer screens as well as their microphone audio.
+						</div>
+					</section>
+				</div>
+				<div class="idi-project fl-col-mixed">
+					<section>
+						<div class="idi-project-logo fl-col-fixed fl-force-left">
+							<a class="idi-no-tab-focus" href="http://sites.google.com/site/decapodproject/">
+								<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/Decapod.png" title="Decapod project logo" alt="Decapod project logo"/>
+							</a>
+						</div>
+						<div class="fl-col-flex">
+							<h3><a href="http://sites.google.com/site/decapodproject/">Decapod</a></h3>
+							Decapod is a project focused on building a low-cost digitization solution that will allow for rare materials, materials held in collections without large budgets, and other scholarly content to be digitized into a high-quality PDF format. This project will work to incorporate the hardware and software necessary to accomplish this goal.
+						</div>
+					</section>
+				</div>
+				<div class="idi-project fl-col-mixed">
+					<section>
+						<div class="idi-project-logo fl-col-fixed fl-force-left">
+							<img class="idi-logo-placeholder" src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/logoplaceholder/logoplaceholder.png" title="Project logo placeholder" alt="Project logo placeholder" />
+						</div>
+						<div class="fl-col-flex">
+							<h3><a href="http://irlt.yorku.ca/">Compareware</a></h3>
+							Compareware is a game for 5-7 year olds that attempts to assist those with reading difficulties to 'level up' their vocabulary and their analytical skills in determining similarities and differences between two objects.
+						</div>
+					</section>
+				</div>
+				<div class="idi-project fl-col-mixed">
+					<section>
+						<div class="idi-project-logo fl-col-fixed fl-force-left">
+							<a class="idi-no-tab-focus" href="http://atutor.ca/">
+								<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/AT_Logo_1_sm.png" title="ATutor logo" alt="ATutor logo"/>
+							</a>
+						</div>
+						<div class="fl-col-flex">
+							<h3><a href="http://atutor.ca/">ATutor</a></h3>
+							ATutor is an Open Source Web-based Learning Management System (LMS) used to develop and deliver online courses.
+						</div>
+					</section>
+				</div>
 			</div>
 			<div class="fl-col">
-				<section class="idi-project fl-col-mixed">
-					<div class="idi-project-logo fl-col-fixed fl-force-left">
-						<img class="idi-logo-placeholder" src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/logoplaceholder/logoplaceholder.png" title="Project logo placeholder" alt="Project logo placeholder" />
-					</div>
-					<div class="fl-col-flex">
-						<h3><a href="http://www.livedescribe.com/">LiveDescribe</a></h3>
-						LiveDescribe is an open source amateur audio description tool that allows users to create custom audio description and upload it to the LiveDescribe wiki. The program automatically detects non-dialogue areas of the media and highlights them to indicate optimal areas for description.
-					</div>
-				</section>
-				<section class="idi-project fl-col-mixed">
-					<div class="idi-project-logo fl-col-fixed fl-force-left">
-						<a class="idi-no-tab-focus" href="http://gpii.net"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/gpii_logo.png" title="GPII logo" alt="GPII logo"/></a>
-					</div>
-					<div class="fl-col-flex">
-						<h3><a href="http://gpii.net">GPII</a></h3>
-						The Global Public Inclusive Infrastructure will combine cloud computing with web and platform services to make accessibility available everywhere.
-					</div>
-				</section>
-				<section class="idi-project fl-col-mixed">
-					<div class="idi-project-logo fl-col-fixed fl-force-left">
-						<a class="idi-no-tab-focus" href="http://collectionspace.org/"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/CollectionSpace.png" title="CollectionSpace logo" alt="CollectionSpace logo"/></a>
-					</div>
-					<div class="fl-col-flex">
-						<h3><a href="http://collectionspace.org/">CollectionSpace</a></h3>
-						CollectionSpace is an open-source collections management application that meets the needs of museums, historical societies, and other collection-holding organizations. CollectionSpace is designed to be configurable to each organization’s needs, serving as a gateway to digital and physical assets across an institution.
-					</div>
-				</section>
-				<section class="idi-project fl-col-mixed">
-					<div class="idi-project-logo fl-col-fixed fl-force-left">
-						<a class="idi-no-tab-focus" href="http://zenit.senecac.on.ca/wiki/index.php/Big_Blue_Button">
-							<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/Bigbluebutton.png" title="Big Blue Button logo" alt="Big Blue Button logo"/>
-						</a>
-					</div>
-					<div class="fl-col-flex">
-						<h3><a href="http://zenit.senecac.on.ca/wiki/index.php/Big_Blue_Button">Accessibility Features for Big Blue Button</a></h3>
-						Big Blue Button is an open source web conferencing system developed primarily for distance education. This project will apply inclusive design in Big Blue Button as HTML5 features are added.
-					</div>
-				</section>
+				<div class="idi-project fl-col-mixed">
+					<section>
+						<div class="idi-project-logo fl-col-fixed fl-force-left">
+							<img class="idi-logo-placeholder" src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/logoplaceholder/logoplaceholder.png" title="Project logo placeholder" alt="Project logo placeholder" />
+						</div>
+						<div class="fl-col-flex">
+							<h3><a href="http://www.livedescribe.com/">LiveDescribe</a></h3>
+							LiveDescribe is an open source amateur audio description tool that allows users to create custom audio description and upload it to the LiveDescribe wiki. The program automatically detects non-dialogue areas of the media and highlights them to indicate optimal areas for description.
+						</div>
+					</section>
+				</div>
+				<div class="idi-project fl-col-mixed">
+					<section>
+						<div class="idi-project-logo fl-col-fixed fl-force-left">
+							<a class="idi-no-tab-focus" href="http://gpii.net"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/gpii_logo.png" title="GPII logo" alt="GPII logo"/></a>
+						</div>
+						<div class="fl-col-flex">
+							<h3><a href="http://gpii.net">GPII</a></h3>
+							The Global Public Inclusive Infrastructure will combine cloud computing with web and platform services to make accessibility available everywhere.
+						</div>
+					</section>
+				</div>
+				<div class="idi-project fl-col-mixed">
+					<section>
+						<div class="idi-project-logo fl-col-fixed fl-force-left">
+							<a class="idi-no-tab-focus" href="http://collectionspace.org/"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/CollectionSpace.png" title="CollectionSpace logo" alt="CollectionSpace logo"/></a>
+						</div>
+						<div class="fl-col-flex">
+							<h3><a href="http://collectionspace.org/">CollectionSpace</a></h3>
+							CollectionSpace is an open-source collections management application that meets the needs of museums, historical societies, and other collection-holding organizations. CollectionSpace is designed to be configurable to each organization’s needs, serving as a gateway to digital and physical assets across an institution.
+						</div>
+					</section>
+				</div>
+				<div class="idi-project fl-col-mixed">
+					<section>
+						<div class="idi-project-logo fl-col-fixed fl-force-left">
+							<a class="idi-no-tab-focus" href="http://zenit.senecac.on.ca/wiki/index.php/Big_Blue_Button">
+								<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/projects/Bigbluebutton.png" title="Big Blue Button logo" alt="Big Blue Button logo"/>
+							</a>
+						</div>
+						<div class="fl-col-flex">
+							<h3><a href="http://zenit.senecac.on.ca/wiki/index.php/Big_Blue_Button">Accessibility Features for Big Blue Button</a></h3>
+							Big Blue Button is an open source web conferencing system developed primarily for distance education. This project will apply inclusive design in Big Blue Button as HTML5 features are added.
+						</div>
+					</section>
+				</div>
 			</div>
 		</div>
