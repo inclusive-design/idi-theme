c5b9ab61a20f2961911526c49b6734acb3912450
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-05-23 18:07:00 -0400

    IDI-71: Fixing layout of infrastructure page

--- infrastructure.php
+++ infrastructure.php
@@ -42,7 +42,8 @@ Template Name: Infrastructure
 						<h2>205 Richmond St. W.<br />Toronto, Canada</h2>
 					</a>
 					
-					<section class="idi-infra-item">
+					<div class="idi-infra-item">
+					<section>
 						<h3>Collaboration Room</h3>
 		<!--   Removed until functionality available
 						<a class="idi-infra-bookit" href="#">Book it</a>
@@ -50,8 +51,10 @@ Template Name: Infrastructure
 						<p class="coming-soon">Booking coming soon</p>
 						<p>The hub of the Distributed Collaboration Network, enabling distributed participation by research team members, supporting resource sharing, collaborative development and both ongoing casual and scheduled communication in multiple modalities.</p>
 					</section>
+					</div>
 				
-					<section class="idi-infra-item">
+					<div class="idi-infra-item">
+					<section>
 						<h3>Networked usability and accessibility labs</h3>
 		<!--   Removed until functionality available
 						<a class="idi-infra-bookit" href="#">Book it</a>
@@ -59,6 +62,7 @@ Template Name: Infrastructure
 						<p class="coming-soon">Booking coming soon</p>
 						<p>A lab offering a full set of assistive technologies and sophisticated synthesis and analysis tools. It is networked to similar facilities at the other 8 institutions as well as to VULab for remote usability testing in real life contexts.</p>
 					</section>
+					</div>
 				</section>
 			</div>
 			
@@ -67,7 +71,8 @@ Template Name: Infrastructure
 					<a class="idi-map-link" href="http://g.co/maps/rzxfr">
 						<h2>230 Richmond St. W.<br />Toronto, Canada</h2>
 					</a>
-					<section class="idi-infra-item">
+					<div class="idi-infra-item">
+					<section>
 						<h3>Sensory Mode Translation and Media Access Facility</h3>
 		<!--   Removed until functionality available
 						<a class="idi-infra-bookit" href="#">Book it</a>
@@ -75,8 +80,10 @@ Template Name: Infrastructure
 						<p class="coming-soon">Booking coming soon</p>
 						<p>An audio/video recording suite enabling research in captioning and description: Supports both post-production and real time captioning and description.</p>
 					</section>
+					</div>
 				
-					<section class="idi-infra-item">
+					<div class="idi-infra-item">
+					<section>
 						<h3>Experimental Inclusive Meeting Facility</h3>
 		<!--   Removed until functionality available
 						<a class="idi-infra-bookit" href="#">Book it</a>
@@ -84,6 +91,7 @@ Template Name: Infrastructure
 						<p class="coming-soon">Booking coming soon</p>
 						<p>An optimally accessible meeting room that supports remote and on-site multi-lingual translation, real time captioning and description, sign language interpretation and a variety of assistive listening devices.</p>
 					</section>
+					</div>
 				</section>
 			</div>
 			
@@ -93,7 +101,8 @@ Template Name: Infrastructure
 						<h2>130 St. George St.<br />Toronto, Canada</h2>
 					</a>
 					
-					<section class="idi-infra-item">
+					<div class="idi-infra-item">
+					<section>
 						<h3>Ubiquitous Web Facility</h3>
 		<!--   Removed until functionality available
 						<a class="idi-infra-bookit" href="#">Book it</a>
@@ -101,6 +110,7 @@ Template Name: Infrastructure
 						<p class="coming-soon">Booking coming soon</p>
 						<p>A facility housing the Mobile and Pervasive Computing research platform, which includes sensors, receivers, transmitters, and a range of both open source or uniquely innovative mobile hardware kits and devices.</p>
 					</section>
+					</div>
 				
 				</section>
 
@@ -109,7 +119,8 @@ Template Name: Infrastructure
 						<h2>49 McCaul St.<br />Toronto, Canada</h2>
 					</a>
 
-					<section class="idi-infra-item">
+					<div class="idi-infra-item">
+					<section>
 						<h3>Accessible Performance Space</h3>
 		<!--   Removed until functionality available
 						<a class="idi-infra-bookit" href="#">Book it</a>
@@ -117,8 +128,10 @@ Template Name: Infrastructure
 						<p class="coming-soon">Booking coming soon</p>
 						<p>A fully accessible performance space, equipped to enable accessible live performances. The space has support for real-time captioning, description, sign translation and fm listening assistance.</p>
 					</section>
+					</div>
 
-					<section class="idi-infra-item">
+					<div class="idi-infra-item">
+					<section>
 						<h3>Participatory Inclusive Design Lab</h3>
 		<!--   Removed until functionality available
 						<a class="idi-infra-bookit" href="#">Book it</a>
@@ -126,6 +139,7 @@ Template Name: Infrastructure
 						<p class="coming-soon">Booking coming soon</p>
 						<p>A lab that provides the collaborative technologies and applications needed to engage diverse design participants with a variety of accessibility needs.</p>
 					</section>
+					</div>
 				</section>
 			</div>
 		</section>
@@ -140,25 +154,29 @@ Template Name: Infrastructure
 	-->
 			</div>
 			<div class="fl-col">
-				<section class="idi-infra-item">
+				<div class="idi-infra-item">
+					<section>
 					<h3>Accessible Gaming Platform</h3>
 	<!--   Removed until functionality available
 					<a class="idi-infra-bookit" href="#">Book it</a>
 	-->
 					<p class="coming-soon">Booking coming soon</p>
 					<p>A range of gaming consoles and peripherals to support researchers in exploring how gaming interfaces can be repurposed to provide more affordable alternative access for individuals with mobility impairments.</p>
-				</section>
+					</section>
+				</div>
 			</div>
 			
 			<div class="fl-col">
-				<section class="idi-infra-item">
+				<div class="idi-infra-item">
+					<section>
 					<h3>Alternative Input Hardware</h3>
 	<!--   Removed until functionality available
 					<a class="idi-infra-bookit" href="#">Book it</a>
 	-->
 					<p class="coming-soon">Booking coming soon</p>
 					<p>Various alternative input devices such as touch screens, enhanced keyboards, key guards, specialized pointing devices, tablets, switch interfaces and joysticks.</p>
-				</section>
+					</section>
+				</div>
 			</div>
 		</section>
 	</div>
