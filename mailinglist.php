953acc116a7c832c4b9e8285099d98d8c5406cfa
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-09-27 13:37:58 -0400

    IDI-102: Fix to the IDI mailing list signup.

--- mailinglist.php
+++ mailinglist.php
@@ -2,7 +2,7 @@
 
 if (isset($_POST['email'])) {
 	$email = trim($_POST['email']);	
-	$subscribeurl="http://lists.idrc.ocad.ca/cgi-bin/mailman/admin/idi-discuss/members/add?subscribe_or_invite=0&send_welcome_msg_to_this_batch=1&notification_to_list_owner=1&subscribees_upload=" . $email . "&adminpw=v0rl0n!";
+	$subscribeurl="http://lists.idrc.ocad.ca/cgi-bin/mailman/admin/idi-discuss/members/add?subscribe_or_invite=0&send_welcome_msg_to_this_batch=1&notification_to_list_owner=1&subscribees_upload=" . $email . "&adminpw=eY3g41632W";
 	if (@file_get_contents("$subscribeurl"))
 		echo $email;
 }
